c6796d2fddf69aed43474ebb8ec48634053d3fc0
snakemuho <snakemuho@gmail.com>
2023-09-12 05:01:21 +0300

    Unit testing

---
+++ RunGroopWebApp.Tests/Controller/AccountControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Controllers;
+using RunGroopWebApp.Data;
+using RunGroopWebApp.Models;
+using RunGroopWebApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunGroopWebApp.Tests.Controller
+{
+    public class AccountControllerTests
+    {
+        private AccountController _accountController;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public AccountControllerTests()
+        {
+            //Dependencies
+            _userManager = A.Fake<UserManager<AppUser>>();
+            _signInManager = A.Fake<SignInManager<AppUser>>();
+            _context = A.Fake<ApplicationDbContext>();
+
+            //SUT
+            _accountController = new AccountController(_userManager, _signInManager, _context);
+        }
+
+        [Fact]
+        public void AccountController_Login_ReturnsSuccess()
+        {
+            //Arrange
+            var response = A.Fake<LoginViewModel>();
+
+            //Act
+            var result = _accountController.Login(response);
+            //Assert
+            result.Should().BeOfType<IActionResult>();
+        }
+    }
+}
---
+++ RunGroopWebApp.Tests/Controller/ClubControllerTests.cs
@@ -0,0 +1,76 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Controllers;
+using RunGroopWebApp.Interfaces;
+using RunGroopWebApp.Models;
+using RunGroopWebApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunGroopWebApp.Tests.Controller
+{
+    public class ClubControllerTests
+    {
+        private ClubController _clubController;
+        private IClubRepository _clubRepository;
+        private IPhotoService _photoService;
+        private HttpContextAccessor _httpContextAccessor;
+
+        public ClubControllerTests()
+        {
+            //Dependencies
+            _clubRepository = A.Fake<IClubRepository>();
+            _photoService = A.Fake<IPhotoService>();
+            _httpContextAccessor = A.Fake<HttpContextAccessor>();
+
+            //SUT
+            _clubController = new ClubController(_clubRepository, _photoService, _httpContextAccessor);
+        }
+
+        [Fact]
+        public void ClubController_Index_ReturnsSuccess()
+        {
+            //Arrange
+            var clubs = A.Fake<IEnumerable<Club>>();
+            A.CallTo(() => _clubRepository.GetAll()).Returns(clubs);
+
+            //Act
+            var result = _clubController.Index();
+            //Assert - Object check actions
+            result.Should().BeOfType<Task<IActionResult>>();
+        }
+
+        [Fact]
+        public void ClubController_Detail_ReturnsSuccess()
+        {
+            //Arrange
+            var id = 1;
+            var club = A.Fake<Club>();
+            A.CallTo(() => _clubRepository.GetByIdAsync(id)).Returns(club);
+            //Act
+            var result = _clubController.Detail(id);
+
+            //Assert
+            result.Should().BeOfType<Task<IActionResult>>();
+        }
+
+        [Fact]
+        public void ClubController_Edit_ReturnsSuccess()
+        {
+            //Arrange
+            var id = 1;
+            var club = A.Fake<Club>();
+            A.CallTo(() => _clubRepository.GetByIdAsync(id)).Returns(club);
+            //Act
+            var result = _clubController.Edit(id);
+
+            //Assert
+            result.Should().BeOfType<Task<IActionResult>>();    
+        }
+    }
+}
